5d347fac37edd3c16ede9f82e15ab40f78cb5b8d
Yan Yermalayeu <yermik2014@yandex.ru>
2023-11-18 00:53:41 +0300

    fix: fix config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,35 +1,30 @@
-const path = require("path")
+import type { StorybookConfig } from "@storybook/react-webpack5"
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin"
 
-module.exports = {
+const config: StorybookConfig = {
+  framework: { name: "@storybook/react-webpack5", options: {} },
   stories: ["../src/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-themes",
+    "@storybook/themes"
   ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
   docs: {
     autodocs: "tag",
   },
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader"],
-      include: path.resolve(__dirname, "../"),
-    })
-
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve("babel-loader"),
-      options: {
-        presets: [["react-app", { flow: false, typescript: true }]],
-      },
-    })
-    config.resolve.extensions.push(".ts", ".tsx")
-
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          extensions: config.resolve.extensions,
+        }),
+      ]
+    }
     return config
   },
 }
+
+export default config
